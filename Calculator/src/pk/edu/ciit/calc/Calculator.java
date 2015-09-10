3658cf851707f11102caab8c470ac8d53ef20445
sajid <sajid.shah.de@gmail.com>
2015-09-10 21:38:36 +0500

    initial commit

---
+++ Calculator/src/pk/edu/ciit/calc/Calculator.java
@@ -0,0 +1,10 @@
+package pk.edu.ciit.calc;
+
+public class Calculator {
+	int add(int num1, int num2){
+		return num1 + num2;
+	}
+	int subtract(int num1, int num2){
+		return num1 - num2;
+	}
+}
---
+++ Calculator/src/pk/edu/ciit/calc/CalculatorApp.java
@@ -0,0 +1,12 @@
+package pk.edu.ciit.calc;
+
+public class CalculatorApp {
+	static Calculator calc = new Calculator();
+
+	public static void main(String[] args) {
+		int a= 10, b=20;
+		System.out.println(calc.add(a, b));
+
+	}
+
+}
